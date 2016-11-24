ecab3bb3c844e9588e711b359a7e00597c542231
siwufeiwu <shellsiwufeiwu@163.com>
2016-11-24 12:58:53 +0800

    ==

--- lib/SpiderCtrl.py
+++ lib/SpiderCtrl.py
@@ -23,34 +23,29 @@ class SpiderCtrl(object):
         self.root_path = os.path.dirname(os.path.dirname(__file__))
         self.cfgctrl = ConfigCtrl()
 
-    def request(self, method, url, data, retry=2):
+    def request(self, method, url, data=None, retry=2):
         if not url or not method:
             return None
+        send_header = {
+            'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+            'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            # 'Accept-Encoding':'gzip, deflate, sdch',
+            'Accept-Language':'zh-CN,zh;q=0.8,en;q=0.6'
+        }
         while retry != 0:
             if method == 'GET':
-                url_value = urllib.urlencode(data)
-                response = urllib2.urlopen(url + '?' + url_value)
-                if response.getcode() != 200:
-                    response = urllib2.urlopen(url + '?' + url_value)
-                else:
-                    break
+                req = urllib2.Request(url + '?' + urllib.urlencode(data))
             elif method == 'POST':
                 url_value = urllib.urlencode(data)
-                send_header = {
-                    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-                    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                    # 'Accept-Encoding':'gzip, deflate, sdch',
-                    'Accept-Language':'zh-CN,zh;q=0.8,en;q=0.6'
-                }
                 req = urllib2.Request(url, data=url_value)
-                req.headers = send_header
-                # req.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.2; rv:16.0) Gecko/20100101 Firefox/16.0')
-                # print req.get_header('User-Agent')
+            req.headers = send_header
+            # req.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.2; rv:16.0) Gecko/20100101 Firefox/16.0')
+            # print req.get_header('User-Agent')
+            response = urllib2.urlopen(req, timeout=60)
+            if response.getcode() != 200:
                 response = urllib2.urlopen(req, timeout=60)
-                if response.getcode() != 200:
-                    response = urllib2.urlopen(req, timeout=60)
-                else:
-                    break
+            else:
+                break
             retry -= 1
         # print response.info()
         return response.read()
@@ -59,7 +54,6 @@ class SpiderCtrl(object):
         # dist_type = sys.getfilesystemencoding()
         dist_type = 'utf-8'
         org_type = chardet.detect(html_cont)['encoding']
-        print org_type, dist_type
         if org_type == 'gkb' or org_type == 'GBK':
             return html_cont.decode('gbk', 'ignore').encode(dist_type)
         elif org_type == 'gb2312' or org_type == 'GB2312':
@@ -83,14 +77,9 @@ class SpiderCtrl(object):
             '_page':1
         }
         new_urls.add(root_url)
-        # links = soup.find_all('a', href=re.compile(r"/view/\d+\.htm"))
-        # for link in links:
-        #     new_url = link['href']
-        #     new_full_url = urlparse.urljoin(page_url, new_url)
-        #     new_urls.add(new_full_url)
 
         while len(new_urls) != 0:
-            html_cont = self.request('POST', new_urls.pop(), data)
+            html_cont = self.request('GET', new_urls.pop(), data)
             # print html_cont
             soup = BeautifulSoup(html_cont, 'html.parser', from_encoding='utf-8')
             # print soup.original_encoding
@@ -100,8 +89,8 @@ class SpiderCtrl(object):
             for node in nodes:
                 node_content = node.find('p', class_='content')
                 node_sub = node.find('p', class_='sub').find_all('span')
-                contact = str(node_sub[1].contents).lstrip('[').rstrip(']')
-                model = str(node_sub[3].contents).lstrip('[').rstrip(']')
+                contact = str(node_sub[1].contents).lstrip('[').rstrip(']').decode('unicode-escape')
+                model = str(node_sub[3].contents).lstrip('[').rstrip(']').decode('unicode-escape')
                 ctime = str(node_sub[4].contents).lstrip('[').rstrip(']').replace('u', '').replace("'", '')
                 ctime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.mktime(datetime.datetime.strptime(ctime,"%Y-%m-%d %H:%M").timetuple())))
                 comment = str(node_content.contents).lstrip('[').rstrip(']').replace('\\r', '').replace('\\n', '').replace('\\t', '').decode('unicode-escape')
